b146508debb548a85d537770728d01e5bd80b9b1
ultrix4ever <71484844+ultrix4ever@users.noreply.github.com>
2022-11-23 11:47:23 +0500

    Add files via upload

--- functions/test.py
+++ functions/test.py
@@ -1,9 +1,3 @@
-n = int(input())
-list1 = []
-list2 = []
-
-for i in range(n):
-    list3 = [int(i) for i in input().split()]
-    list2 += list3
-    
-print(list2)
+name = input()
+name = name[0].upper()
+print(name)
