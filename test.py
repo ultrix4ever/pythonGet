944451eda2d73f8b4c7ea5971b4eb1608a23a83a
vitaly_rabets <vitaly_rabets@office.targem.ru>
2023-03-03 10:43:24 +0500

    123

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-a = str(123456789)
-lst = list(a)
-lst2 = []
+x = 7 // (-6)
+print(x)
 
-for i in range(-1,-len(a),-3):
-    lst2 += lst[i:i-3:-1]+[',']
-    print(lst2)
-lst2 = lst2[::-1]
-if lst2[0] == ',':
-    print(*lst2[1:], sep='')
-else: 
-    print(*lst2, sep='')
+7 = -2 * -6 + (-5)
