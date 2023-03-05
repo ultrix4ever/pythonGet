00fdfd40ade5508acea3a5f4ad9f2bfe4489578d
vitaly_rabets <vitaly_rabets@office.targem.ru>
2023-03-05 15:47:19 +0500

    123

---
+++ advanced crs/Type bool/bool311.py
@@ -0,0 +1,15 @@
+
+
+def func(num1, num2):
+    if num1%num2==0:
+        return 'делится'
+    else:
+        return 'не делится'
+
+num1, num2 = int(input()), int(input())
+
+print(func(num1, num2))
+
+#x = num1/num2
+#print(x)        
+
