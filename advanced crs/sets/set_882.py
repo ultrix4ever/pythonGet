35405def132fb5d51dfa546a0e120f19a1c4b43b
vitaly_rabets <vitaly_rabets@office.targem.ru>
2023-09-03 18:06:33 +0500

    123

---
+++ advanced crs/sets/set_882.py
@@ -0,0 +1,3 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+myset = set(i[0].lower() for i in words)
+print(*sorted(myset))
