95164d19db9f93caa3ca09d9816b31e5ac8a3d1b
vitaly_rabets <vitaly_rabets@office.targem.ru>
2023-10-11 17:00:56 +0500

    new lessons of dict

---
+++ advanced crs/dict/dev.py
@@ -0,0 +1,22 @@
+capitals = {'Россия': 'Москва', 'Франция': 'Париж', 'Чехия': 'Прага'}
+
+# for key in capitals:
+#     print(key)
+
+
+# for key in capitals:
+#     print(capitals[key])
+
+# for key in capitals:
+#     print('Столица', key, '- это', capitals[key])
+
+# for key in capitals.keys():     # итерируем по списку ['Россия', 'Франция', 'Чехия']
+#     print(key)
+
+# print(*capitals.keys(), sep='\n')
+
+print(capitals.items())
+
+# for key, value in capitals.items():
+#     print(key, '-', value)
+
---
+++ advanced crs/dict/dict1024.py
@@ -0,0 +1,6 @@
+list_numbers = [int(i) for i in input()]
+dict_numbers = {0:'zero', 1:'one', 2:'two', 3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine' }
+
+for i in list_numbers:
+    print(dict_numbers[i], end=' ')
+
